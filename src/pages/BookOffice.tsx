8fb9c4594ec04cf70804f35f3ae8ce5993b8172a
Muhamad Iksan <muhamadiksan351@gmail.com>
2025-01-27 22:43:54 +0700

    update: success booking

--- src/pages/BookOffice.tsx
+++ src/pages/BookOffice.tsx
@@ -4,6 +4,7 @@ import { Office } from '../types/types';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 import { z } from 'zod';
+import { bookingSchema } from '../types/ValidationBooking';
 
 export default function BookOffice() {
   const { slug } = useParams<{ slug: string }>();
@@ -22,6 +23,7 @@ export default function BookOffice() {
   });
 
   const [formErrors, setFormErrors] = useState<z.ZodIssue[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const [uniqueCode, setUniqueCode] = useState<number>(0);
   const [totalAmountWithUniqueCode, setTotalAmountWithUniqueCode] = useState<number>(0);
@@ -91,6 +93,58 @@ export default function BookOffice() {
     });
   };
 
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log('Validating form data...');
+    const validation = bookingSchema.safeParse(formData);
+
+    console.log(formData);
+
+    if (!validation.success) {
+      console.error('Validation errors:', validation.error.issues);
+      setFormErrors(validation.error.issues);
+      return;
+    }
+
+    console.log('Form data is valid. Submitting...', formData);
+
+    setIsLoading(true);
+
+    try {
+      const response = await axios.post(
+        'http://127.0.0.1:8000/api/booking-transaction',
+        {
+          ...formData,
+        },
+        {
+          headers: {
+            'X-API-KEY': '1234',
+          },
+        }
+      );
+
+      console.log('Form submitted successfully:', response.data);
+
+      navigate('/success-booking', {
+        state: {
+          office,
+          booking: response.data.data,
+        },
+      });
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        console.error('Error submitting form:', error.message);
+        setError(error.message);
+      } else {
+        console.error('Unexpected error:', error);
+        setError('An unexpected error occurred');
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   const baseURL = 'http://127.0.0.1:8000/storage';
 
   return (
@@ -101,7 +155,7 @@ export default function BookOffice() {
         <div className="absolute w-full h-full bg-[linear-gradient(180deg,_rgba(0,0,0,0)_0%,#000000_91.83%)] z-10" />
         <img src="/assets/images/thumbnails/thumbnail-details-4.png" className="absolute w-full h-full object-cover object-top" alt="" />
       </div>
-      <form action="booking-finished.html" className="relative flex justify-center max-w-[1130px] mx-auto gap-[30px] mb-20 z-20">
+      <form onSubmit={handleSubmit} action="booking-finished.html" className="relative flex justify-center max-w-[1130px] mx-auto gap-[30px] mb-20 z-20">
         <div className="flex flex-col shrink-0 w-[500px] h-fit rounded-[20px] border border-[#E0DEF7] p-[30px] gap-[30px] bg-white">
           <div className="flex items-center gap-4">
             <div className="flex shrink-0 w-[140px] h-[100px] rounded-[20px] overflow-hidden">
@@ -124,8 +178,17 @@ export default function BookOffice() {
               </label>
               <div className="flex items-center rounded-full border border-[#000929] px-5 gap-[10px] transition-all duration-300 focus-within:ring-2 focus-within:ring-[#0D903A]">
                 <img src="/assets/images/icons/security-user-black.svg" className="w-6 h-6" alt="icon" />
-                <input type="text" name="name" id="name" className="appearance-none outline-none w-full py-3 font-semibold placeholder:font-normal placeholder:text-[#000929]" placeholder="Write your complete name" />
+                <input
+                  value={formData.name}
+                  onChange={handleChange}
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="appearance-none outline-none w-full py-3 font-semibold placeholder:font-normal placeholder:text-[#000929]"
+                  placeholder="Write your complete name"
+                />
               </div>
+              {formErrors.find((error) => error.path.includes('name')) && <p className="text-red-500">Name is required</p>}
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="phone" className="font-semibold">
@@ -133,8 +196,17 @@ export default function BookOffice() {
               </label>
               <div className="flex items-center rounded-full border border-[#000929] px-5 gap-[10px] transition-all duration-300 focus-within:ring-2 focus-within:ring-[#0D903A]">
                 <img src="/assets/images/icons/call-black.svg" className="w-6 h-6" alt="icon" />
-                <input type="tel" name="phone" id="phone" className="appearance-none outline-none w-full py-3 font-semibold placeholder:font-normal placeholder:text-[#000929]" placeholder="Write your valid number" />
+                <input
+                  value={formData.phone_number}
+                  onChange={handleChange}
+                  type="tel"
+                  name="phone_number"
+                  id="phone"
+                  className="appearance-none outline-none w-full py-3 font-semibold placeholder:font-normal placeholder:text-[#000929]"
+                  placeholder="Write your valid number"
+                />
               </div>
+              {formErrors.find((error) => error.path.includes('phone_number')) && <p className="text-red-500">Phone Number is required</p>}
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="date" className="font-semibold">
@@ -143,12 +215,15 @@ export default function BookOffice() {
               <div className="flex items-center rounded-full border border-[#000929] px-5 gap-[10px] transition-all duration-300 focus-within:ring-2 focus-within:ring-[#0D903A] overflow-hidden">
                 <img src="/assets/images/icons/calendar-black.svg" className="w-6 h-6" alt="icon" />
                 <input
+                  value={formData.started_at}
+                  onChange={handleChange}
                   type="date"
-                  name="date"
+                  name="started_at"
                   id="date"
                   className="relative appearance-none outline-none w-full py-3 font-semibold [&::-webkit-calendar-picker-indicator]:absolute [&::-webkit-calendar-picker-indicator]:w-full [&::-webkit-calendar-picker-indicator]:h-full [&::-webkit-calendar-picker-indicator]:opacity-0"
                 />
               </div>
+              {formErrors.find((error) => error.path.includes('started_at')) && <p className="text-red-500">Started At is required</p>}
             </div>
           </div>
           <hr className="border-[#F6F5FD]" />
@@ -230,8 +305,8 @@ export default function BookOffice() {
             </div>
           </div>
           <hr className="border-[#F6F5FD]" />
-          <button type="submit" className="flex items-center justify-center w-full rounded-full p-[16px_26px] gap-3 bg-[#0D903A] font-bold text-[#F7F7FD]">
-            <span>I’ve Made The Payment</span>
+          <button disabled={isLoading} type="submit" className="flex items-center justify-center w-full rounded-full p-[16px_26px] gap-3 bg-[#0D903A] font-bold text-[#F7F7FD]">
+            <span>{isLoading ? 'Loading...' : "I've already paid"}</span>
           </button>
         </div>
       </form>
